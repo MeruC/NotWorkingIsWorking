72ab06e89864751035f93a089d290ab9c5a53e8f
MeruC <carmelomelvincent@gmail.com>
2023-09-17 17:57:21 +0800

    stuff

--- scenes/level_create_Menu/level_create.gd
+++ scenes/level_create_Menu/level_create.gd
@@ -1,7 +1,9 @@
 extends Control
 
-onready var width = get_node("/root/level_create/size/w")
-onready var depth = get_node("/root/level_create/size/d")
+export( NodePath ) onready var width = get_node(width) as SpinBox
+export( NodePath ) onready var depth = get_node(depth) as SpinBox
+export( NodePath ) onready var playarea = get_node(playarea) as CSGBox
+export( NodePath ) onready var camera = get_node(camera) as Camera
 
 var cur_w = 10
 var cur_d = 10
@@ -19,19 +21,26 @@ func _on_create_pressed():
 	Global.w = cur_w
 	Global.d = cur_d
 	get_tree().change_scene_to(load("res://online_mode/level_editor/level_editor.tscn"))
-	pass # Replace with function body.
 
 
 func _on_w_value_changed(value):
 	cur_w = value
-	pass # Replace with function body.
+	playarea.set_width(cur_w * 2)
+	if cur_w >= cur_d:
+		camera.fov = cur_w
+	else:
+		camera.fov = cur_d
+			
 
 
 func _on_d_value_changed(value):
 	cur_d = value
-	pass # Replace with function body.
+	playarea.set_depth(cur_d * 2)
+	if cur_w >= cur_d:
+		camera.fov = cur_w
+	else:
+		camera.fov = cur_d
 
 
 func _on_inventory_test_pressed():
 	get_tree().change_scene_to(load("res://nodes/inventory.tscn"))
-	pass # Replace with function body.
