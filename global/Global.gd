8ee0d83f821a18e80a0b839796dbce5e40ed3e30
MeruC <carmelomelvincent@gmail.com>
2023-09-20 00:41:22 +0800

    ESC to go to main menu

--- global/Global.gd
+++ global/Global.gd
@@ -6,6 +6,7 @@ var edit_mode = true
 var can_place = true
 var is_usingJoystick = false
 var just_onMenu = false
+var main_screen = "res://scenes/main_screen/main_screen.tscn"
 
 var w = 10
 var d = 10
@@ -17,3 +18,7 @@ func _ready():
 	
 var editor_mode = "place"
 var e_mode_history = "place"
+
+func _input(event):
+	if(Input.is_action_just_pressed("main_menu")):
+		get_tree().change_scene(main_screen)
