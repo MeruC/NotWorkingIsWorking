efc5717eb212625afecee72e65d87f47f158ba3c
MeruC <carmelomelvincent@gmail.com>
2023-09-20 17:37:07 +0800

    shop stuff

--- scenes/main_shop/shop_option.gd
+++ scenes/main_shop/shop_option.gd
@@ -3,57 +3,60 @@ extends InnerPanel
 export ( NodePath ) onready var price_list = get_node( price_list ) as VBoxContainer
 export ( NodePath ) onready var produce_list = get_node( produce_list ) as VBoxContainer
 export ( NodePath ) onready var craft_btn = get_node( craft_btn ) as Button
+export ( NodePath ) onready var price_lbl = get_node( price_lbl ) as Label
+export ( String ) var panel_title
+export ( int ) var price
+export ( String ) var item_id
+export ( int ) var quantity
 
-var price = [{
-	"id": "gold_coin", 
-	"quantity": 50
-}]
+onready var produce = [{
+		"id": item_id, 
+		"quantity": quantity
+	}]
 
-var produce = [{
-	"id": "rj45", 
-	"quantity": 1
-}]
 
 var recipe_id = "RJ45"
 
-export( Resource ) var settings_data
-onready var coins = $"../../../../../player_stats/VBoxContainer/coins/Label2"
+onready var coins = $"../../../../../player_stats/VBoxContainer/coins/player_coins"
 
+export( Resource ) var settings_data
 
 func _ready():
 	SignalManager.connect( "inventory_group_content_changed", self, "_on_inventory_group_changed" )
-	
-
-# Set the price and produce list.
-func set_info( recipe_id, price_items, produce_items ):
-	#price = price_items
-	#produce = produce_items
-	set_title( recipe_id )
+	set_title(panel_title)
+	price_lbl.text = str(price)
+	set_info(produce)
 	set_craft_button()
 	
-	for item_data in price_items:
-		var price_node = ResourceManager.get_instance( "item_quantity" )
-		price_list.add_child( price_node )
-		var item = ItemManager.get_item( item_data.id )
-		price_node.set_info( item, item_data.quantity )
+
+
+# Set the price and produce list.
+func set_info( produce_items ):
+	#price = price_items
+	produce = produce_items
+	
 	
 	for item_data in produce_items:
 		var produce_node = ResourceManager.get_instance( "item_quantity" )
 		produce_list.add_child( produce_node )
 		var item = ItemManager.get_item( item_data.id )
+		print(ItemManager.get_item( item_data.id ))
 		produce_node.set_info( item, item_data.quantity )
 
 # Activate the craft button if the 'crafting' inventories has the needed items
 # and the 'player' inventories has enough space for the produced items.
 func set_craft_button():
-	var can_craft = int(coins.text) >= 50
+	print(settings_data.gold_coins)
+	var can_craft = settings_data.gold_coins >= 50
 	craft_btn.disabled = not can_craft
 	pass
 
 # When crafting, remove the price tiems, adds the produces.
 func _on_craft_pressed():
 	var current_coins = int(coins.text)
-	coins.text = str(current_coins - 50)
+	if current_coins < price:
+		pass
+	coins.text = str(current_coins - price)
 	settings_data.gold_coins = int(coins.text)
 	print(settings_data.gold_coins)
 	InventoryManager.add_items( ItemManager.get_items( produce ), "player" )
