1d6de7038205599ff7c9aa010b72b4bf3fe73edb
SHIR00101 <120690550+SHIR00101@users.noreply.github.com>
2023-09-26 17:43:06 +0800

    backup bago ang sakuna

--- global/chapters/level_selection_camera.gd
+++ global/chapters/level_selection_camera.gd
@@ -9,6 +9,9 @@ var reachlevel2 = false
 
 onready var level1 = $"../level1"
 onready var level2 = $"../level2"
+onready var level3 = $"../level3"
+onready var count = 0
+
 export var max_speed = 10
 export var friction = 100 # Replace with your desired position
 var showDistanceThreshold = 1.0  # Adjust as needed
@@ -21,9 +24,6 @@ func _ready():
 	pass
 
 func _process(delta: float) -> void:
-	if reachlevel2:
-		return
-
 	var direction = get_swipe_direction()
 	apply_friction(direction, delta)
 	apply_movement(direction, delta)
@@ -32,30 +32,53 @@ func _process(delta: float) -> void:
 
 	# Check if the new position is within the bounds
 	if is_within_bounds(newCameraPosition):
-		# Update the camera's position
+
 		cameraTransform.origin = newCameraPosition
 		global_transform = cameraTransform
 		
 	#position of every level
-	var targetlevel1 = Vector3(-4.42804, 9.84598, 6.40031) 
-	var targetlevel2 = Vector3(0,0,0)
+	var targetlevel1 = Vector3(-4.42804, 12.2492, 6.40031) 
+	var targetlevel2 = Vector3(4 ,12.2492, 6.40031)
+	var targetlevel3 = Vector3(19.983038, 12.2492, 6.40031)
+	
+	var street2 = Vector3(-4.404224, 12.2492, 15.870784)
+	var street2_1 = Vector3(19.995716, 12.2492, 14.397369)
+	
+	var targetlevel4 = Vector3(15, 12.2492, 15)
 	
 	var cameraPosition = cameraTransform.origin
 	var distanceTolevel1 = cameraPosition.distance_to(targetlevel1)
-	var distantTolevel2 = cameraPosition.distance_to(level2_position)
-
+	var distanceTolevel2 = cameraPosition.distance_to(targetlevel2)
+	var distanceTolevel3 = cameraPosition.distance_to(targetlevel3)
+	var distanceTolevel4 = cameraPosition.distance_to(targetlevel4)
+	var distanceTostreet2 = cameraPosition.distance_to(street2)
+	var distanceTostreet2_1 = cameraPosition.distance_to(street2_1)
 	if distanceTolevel1 < showDistanceThreshold:
-		level1.visible = true
-		
-	elif distantTolevel2 < showDistanceThreshold:
+		level1.visible = true	
+	elif distanceTolevel2 < showDistanceThreshold:
 		# Show the label when close to level2_position
-		level1.visible = true
-		reachlevel2 = true
+		level2.visible = true
+		swipe_direction = Vector3.ZERO
+	elif distanceTolevel3 < showDistanceThreshold:
+		level3.visible = true
+	elif distanceTostreet2 < showDistanceThreshold || distanceTostreet2_1 < showDistanceThreshold:
+		swipe_direction = Vector3.ZERO
+		count += 1
+		if count == 1:
+			$"../CanvasLayer/AnimationPlayer".play("street2_opening")
+			$"../CanvasLayer".visible = true
+		
+	elif distanceTolevel4 < showDistanceThreshold:
+		$"../level4".visible = true
+		swipe_direction = Vector3.ZERO
+	
 	else:
 		# Hide the label when not close to either position
-		reachlevel2 = false
 		level1.visible = false
-
+		level2.visible = false
+		level3.visible = false
+		$"../level4".visible = false
+		$"../CanvasLayer".visible = false
 # Swipe Gesture Detection
 func _input(event):
 	if not event is InputEventScreenTouch:
@@ -109,3 +132,5 @@ func is_within_bounds(position):
 	)
 	swipe_direction = Vector3.ZERO
 
+
+	
--- global/chapters/level_selector_manager.gd
+++ global/chapters/level_selector_manager.gd
@@ -1,23 +1,33 @@
 extends Spatial
 export (Resource) var settings_data 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$level2.disabled = true
+	$level3.disabled = true
+	$level4.disabled = true
 	if settings_data.level1 == "complete":
-		$level_container/level2.disabled = false
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+		$level2.disabled = false
+	elif settings_data.level2 == "complete":
+		$level3.disabled = false
+	elif settings_data.level3 == "complete":
+		$level4.disabled = false
+	
 
 
 func _on_level1_pressed():
 	Load.load_scene(self, "res://offline_levels/level1/level1_discussion/level1_discussion.tscn")
+	$".".queue_free()
+
+
+func _on_level2_pressed():
+	Load.load_scene(self, "res://offline_levels/level2/level2_discussion/level2_discussion.tscn")
+	$".".queue_free()
+
+func _on_level3_pressed():
+	Load.load_scene(self, "res://offline_levels/level3/level3_discussion.tscn")
+	$".".queue_free()
+
+func _on_level4_pressed():
+	Load.load_scene(self, "res://offline_levels/level4/level4_discussion/level4_discussion.tscn")
 	$".".queue_free()
