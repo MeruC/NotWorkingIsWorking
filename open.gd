0d8cf834dd27edebf102e4b323d20c339eda6142
SHIR00101 <120690550+SHIR00101@users.noreply.github.com>
2023-09-30 20:28:16 +0800

    checking save.dat done

--- open.gd
+++ open.gd
@@ -1,10 +1,13 @@
 extends Node
 
-export (Resource) var settings_data
 
 func _ready():
-	if settings_data.account_status == "old":
-		yield(get_tree().create_timer(1),"timeout")
+	var file2Check = File.new()
+	var pathToCheck = "user://save_data/save.dat"
+	var doFileExists = file2Check.file_exists(pathToCheck)
+
+	if doFileExists:
+		yield(get_tree().create_timer(1), "timeout")
 		Load.load_scene(self, "res://scenes/main_screen/main_screen.tscn")
 	else:
 		Load.load_scene(self, "res://scenes/log_in_screen/login_signup_scene.tscn")
