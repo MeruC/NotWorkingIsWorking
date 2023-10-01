3602b3f845b1fc3a52a1b0c4e453e5598eb41675
MeruC <carmelomelvincent@gmail.com>
2023-10-01 21:55:15 +0800

    defaults

--- open.gd
+++ open.gd
@@ -10,4 +10,8 @@ func _ready():
 		yield(get_tree().create_timer(1), "timeout")
 		Load.load_scene(self, "res://scenes/main_screen/main_screen.tscn")
 	else:
+		OS.window_fullscreen = true
+		var viewport = get_viewport()
+		viewport.size.x = 1280
+		viewport.size.y = 720
 		Load.load_scene(self, "res://scenes/log_in_screen/login_signup_scene.tscn")
