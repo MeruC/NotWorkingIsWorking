7aaf769394e23492cddfd09e22bc605fc044d496
sjfajarda <stephenjoy.fajarda.a@bulsu.edu.ph>
2023-11-08 12:49:49 +0800

    updatesss

---
+++ offline_levels/level9/tutorial_manager.gd
@@ -0,0 +1,50 @@
+extends Control
+
+onready var video_player = $"../VideoPlayer"
+var configure_router = preload("res://resources/video_tutorials/configure_router.webm")
+var connecting_devices = preload("res://resources/video_tutorials/connecting_devices.webm")
+var ip_configuration = preload("res://resources/video_tutorials/ip_configuration.webm")
+var ping_device = preload("res://resources/video_tutorials/ping_device.webm")
+
+func _on_connect_devices_pressed():
+	video_player.set_stream(connecting_devices)
+	video_player.visible = true
+	video_player.play()
+
+
+func _on_configure_ip_pressed():
+	video_player.set_stream(ip_configuration)
+	video_player.visible = true
+	video_player.play()
+
+
+func _on_ping_device_pressed():
+	video_player.set_stream(ping_device)
+	video_player.visible = true
+	video_player.play()
+
+
+func _on_configure_router_pressed():
+	video_player.set_stream(configure_router)
+	video_player.visible = true
+	video_player.play()
+
+
+func _on_VideoPlayer_finished():
+	video_player.visible = false
+
+
+func _on_back_button_pressed():
+	video_player.stop()
+	video_player.visible = false
+
+
+func _on_tap_pressed():
+	self.visible = false
+
+
+func _on_open_tutorials_toggled(button_pressed):
+	if button_pressed:
+		self.visible = true
+	else:
+		self.visible = false
